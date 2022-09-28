29b3c33c4b484e312b8823f821139d908690f888
farhan sharif <farhansharif1410@gmail.com>
2022-09-28 08:07:06 +0530

    some code

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Cart from '../Cart/Cart';
+import Player from '../Player/Player';
 import './Main.css'
 
 const Main = () => {
     const [search, setSearch] = useState('')
-    const [player, setPlayer] = useState([])
-    // console.log(search);
+    const [players, setPlayers] = useState([])
+    const [cart, setCart] = useState([])
 
     useEffect(() => {
         fetch(`https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=Danny%20Welbeck
-        https://www.thesportsdb.com/api/v1/json/{APIKEY}/searchplayers.php?t={TeamName}&p`)
+        https://www.thesportsdb.com/api/v1/json/{APIKEY}/searchplayers.php?t={TeamName}&p=${search}`)
             .then(res => res.json())
-            .then(data => setPlayer(data?.player))
-    }, [])
+            .then(data => setPlayers(data?.player))
+    }, [search])
 
-    console.log(player);
+
 
     return (
         <div className='main-container'>
             <div className='left-site'>
                 <input onChange={(e) => setSearch(e.target.value)} className='search' type="text" />
                 <button className='search-btn'>Search</button>
+                <div className='players-container'>
+                    {
+                        players.map(player => <Player key={player.idPlayer} player={player} cart={cart} setCart={setCart} />)
+                    }
+                </div>
             </div>
             <div className='right-site'>
-                <h1>right site</h1>
+                <h3 className='item-length'>Added cart:</h3>
+                <div>
+
+                </div>
             </div>
         </div>
     );
---
+++ src/component/Player/Player.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Player.css'
+
+const Player = ({ player, cart, setCart }) => {
+    // console.log(player);
+    const { strPlayer, strThumb, strNationality, strTeam } = player;
+
+    const showData = () => {
+        const info = {
+            strPlayer,
+            strNationality,
+            strTeam,
+            price: 1500
+        }
+        const j = [info]
+        setCart(j)
+        console.log(cart);
+    }
+
+
+    return (
+        <div className='player'>
+            <img className='img' src={strThumb} alt="" />
+            <h4>{strPlayer}</h4>
+            <button className='data-btn'>Show data</button>
+            <button onClick={showData} className='data-btn'>Add data</button>
+            <button className='data-btn'>others</button>
+        </div>
+    );
+};
+
+export default Player;
